033bc7f636b2cd8e53c5b78cf4c4def4cd853724
maria-mes <maramessay655@gmail.com>
2025-12-13 19:13:39 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -16,7 +16,7 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Customers (
+CREATE TABLE Customer (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
