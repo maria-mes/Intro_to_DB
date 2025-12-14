c4fdaf8e370b348a095ac506f93a57e9e3012d2f
maria-mes <maramessay655@gmail.com>
2025-12-14 20:19:20 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,8 +1,8 @@
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+-- task_6.sql
+-- Insert multiple rows into the customer table in alx_book_store
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
-
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
